b9aebd23da2a6b8329d876a51127eaf04157f9bf
ThGox-06 <thgimenez06@gmail.com>
2023-09-17 05:09:07 -0300

    second-commit

--- js/function.js
+++ js/function.js
@@ -1,38 +1,26 @@
-function addNumber(event){
-  // Get input
+// Función para manejar los clics en los botones
+function addNumber(event) {
+  // Obtiene el elemento de entrada
   let input = document.getElementById("myInput");
 
-  // Set button 1
-  let button1 = document.getElementById("button1");
-  button1.removeEventListener("click", addNumber);
-  button1.addEventListener("click", addNumber);
+  // Obtiene el valor numérico del botón clickeado o el operador
+  const clickedButtonValue = event.target.getAttribute("data-value");
 
-  // Set button 2
-  let button2 = document.getElementById("button2");
-  button2.removeEventListener("click", addNumber);
-  button2.addEventListener("click", addNumber);
-  
-  // Clear button
-  let clearButton = document.getElementById("clearButton")
-  clearButton.addEventListener("click", clear)
-
-  // Detects what button was pressed
-  const clickedButton = event.target;
-    
-  // Operation
-  switch(clickedButton){
-    case button1:
-      input.value +=1;
-      break;
-    case button2:
-      input.value +=2;
-      break;
-    default:
-      console.log("default");
-  }
-
-  function clear(){
+  if (clickedButtonValue !== null) {
+    // Si el botón clickeado tiene un valor válido, lo agrega al campo de entrada
+    input.value += clickedButtonValue;
+  } else {
+    // Si no tiene un valor válido, borra el campo de entrada
     input.value = null;
   }
-
 }
+
+// Configura los botones para que utilicen la función addNumber
+document.getElementById("button1").addEventListener("click", addNumber);
+document.getElementById("button2").addEventListener("click", addNumber);
+document.getElementById("plusButton").addEventListener("click", addNumber);
+
+// Configura el botón "Clear" para que borre el campo de entrada
+document.getElementById("clearButton").addEventListener("click", function () {
+  document.getElementById("myInput").value = "";
+});
